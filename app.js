2c6e7f82b54208d0339df9feff0133a0b9cf8c64
0usseynou <papa.camara2@groupe-esigelec.org>
2026-05-11 20:03:25 +0100

    modification ligne 496

--- app.js
+++ app.js
@@ -493,7 +493,7 @@ function renderParties(){
             <td>${p.kills}</td>
             <td>${p.statut}</td>
             <td>
-                <form action = "http://192.168.1.5:8000/stop" method = "POST">
+                <form action = "http://192.168.1.5:8000/stop" method = "POST" target="_blank">
                 <button class="btn-danger" onclick="deletePartie(${p.id})">Supprimer</button>
                 </form>
             </td>
