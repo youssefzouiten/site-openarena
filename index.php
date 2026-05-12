e6dd74a3e548348a274b81b500e0434d7860ac5d
0usseynou <papa.camara2@groupe-esigelec.org>
2026-05-12 19:04:23 +0000

    modif

--- index.php
+++ index.php
@@ -212,11 +212,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 flash('success', 'Partie lancée.');
                 redirectTo('admin');
             }
-            else if (isset($result['status']) )
-                {
+           // else if (isset($result['status']) && $result['status'] === 'erreur')
+             //   {
                     flash('error', 'Partie non lancée.');
                     redirectTo('admin');
-                }
+               // }
         }
 
         if ($action === 'delete_partie') {
