8010b6045f4dbebfac92f909bee6ffac1cec7aa8
0usseynou <papa.camara2@groupe-esigelec.org>
2026-05-12 18:04:03 +0000

    modif

--- index.php
+++ index.php
@@ -228,11 +228,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             curl_close($ch);
 
             $result = json_decode($response, true);
-            if (isset($result['status']) && $result['status'] === 'erreur'){
+          /*  if (isset($result['status']) && $result['status'] === 'erreur'){
                 flash('error', 'Partie non supprimée.');
                 redirectTo('admin');
             }
-          /*  else {
+            else {
                 $stmt = $pdo->prepare("DELETE FROM parties WHERE id = ?");
                 $stmt->execute([(int)($_POST['id'] ?? 0)]);
                 flash('success', 'Partie supprimée.');
