ca0d534b29930d9fd40a45605807bda3cffad459
0usseynou <papa.camara2@groupe-esigelec.org>
2026-05-13 15:45:06 +0000

    modif

--- index.php
+++ index.php
@@ -771,17 +771,6 @@ $maps = ['oa_dm3' => 'oa_dm3 — Duels', 'am_lavactf' => 'am_lavactf — CTF ave
         <!-- Aucun tournoi en cours -->
         <form method="POST">
             <input type="hidden" name="action" value="creer_tournoi">
-            <div class="dropdown">
-  <a class="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-    Dropdown link
-  </a>
-
-  <ul class="dropdown-menu">
-    <li><a class="dropdown-item" href="#">Action</a></li>
-    <li><a class="dropdown-item" href="#">Another action</a></li>
-    <li><a class="dropdown-item" href="#">Something else here</a></li>
-  </ul>
-</div>
             <button type="submit" class="btn-primary" style="padding:12px 25px;">
                 🏆 Créer un nouveau tournoi (8 joueurs)
             </button>
