47d8ef43a398833563707dfa3601a43dd756d3c7
0usseynou <papa.camara2@groupe-esigelec.org>
2026-05-18 11:11:04 +0000

    modif

--- ad.php
+++ ad.php
@@ -2,7 +2,7 @@
 
 function ad_login($pseudo, $password) {
 
-    $ldap = ldap_connect("ldap://192.168.50.30");
+    $ldap = ldap_connect("ldap://192.168.1.200");
 
     ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
     ldap_set_option($ldap, LDAP_OPT_REFERRALS, 0);
@@ -25,7 +25,7 @@ function ad_create_user($pseudo, $email) {
     $adminUser = "Administrator@openarena.local";
     $adminPass = "Group4_";
 
-    $ldap = ldap_connect("ldap://192.168.50.30");
+    $ldap = ldap_connect("ldap://192.168.1.200");
 
     ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
     ldap_set_option($ldap, LDAP_OPT_REFERRALS, 0);
