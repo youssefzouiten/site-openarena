ec46cc3d2bfc99691bd26bec08072f859fa2d6a7
YoussefZouiten74 <youssef.zouiten@groupe-esigelec.org>
2026-05-19 11:08:11 +0200

    config

--- config.php
+++ config.php
@@ -1,14 +1,23 @@
 <?php
-$host = '127.0.0.1';
+
+$host = 'localhost';
 $dbname = 'openarena';
-$user = 'root';
-$pass = '';
+$user = 'openarena_user';
+$pass = 'OpenArena123!';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass);
+
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        $user,
+        $pass
+    );
+
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-} catch (PDOException $e) {
-    die('Erreur connexion BDD : ' . htmlspecialchars($e->getMessage(), ENT_QUOTES, 'UTF-8'));
+
+} catch(PDOException $e){
+
+    die("Erreur BDD : " . $e->getMessage());
 }
+
 ?>
